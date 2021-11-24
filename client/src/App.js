ad76d6875a20dd95d9011b4217e511851a66d9c5
Bears <evanbero1980@gmail.com>
2021-11-24 11:08:50 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -18,14 +18,12 @@ function App() {
           <Container>
             <NavTabs />
             <Routes>
-              <Switch>
-                <Route path='/' element={<Home />} />
-                <Route path='/connect' element={<Connect />} />
-                <Route path='/events' element={<Events />} />
-                <Route path='/giving' element={<Giving />} />
-                <Route path='/staff' element={<Staff />} />
-                <Route path='/watchlive' element={<WatchLive />} />
-              </Switch>
+              <Route path='/' element={<Home />} />
+              <Route path='/connect' element={<Connect />} />
+              <Route path='/events' element={<Events />} />
+              <Route path='/giving' element={<Giving />} />
+              <Route path='/staff' element={<Staff />} />
+              <Route path='/watchlive' element={<WatchLive />} />
             </Routes>
           </Container>
         </main>
