d1645e600faa32c6ed4ab5bf8d0eb015fcdb9802
Bears <evanbero1980@gmail.com>
2021-11-24 08:12:48 -0600

    server bugfix

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ const app = express()
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.js'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 } else {
   app.get('/', (req, res) => {
