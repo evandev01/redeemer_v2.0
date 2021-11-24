6e09edd6e664e805a0c25855a24a3c8fc635af45
Bears <evanbero1980@gmail.com>
2021-11-24 09:00:30 -0600

    static assets bugfix

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,11 @@ const app = express()
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-} else {
-  app.get('/', (req, res) => {
-    res.send('Redeemer API is running')
-  })
+  app.use(express.static('client/build'))
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
