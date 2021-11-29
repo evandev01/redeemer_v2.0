77e582e2d10bdfbf0783ea033d41bd623ab2f3d2
Bears <evanbero1980@gmail.com>
2021-11-29 11:03:05 -0600

    added procfile, edited server

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
   )
