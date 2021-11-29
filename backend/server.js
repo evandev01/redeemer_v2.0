a62ffb7b16cb66f51f00afd76a205c6bef754fe9
Bears <evanbero1980@gmail.com>
2021-11-29 12:12:24 -0600

    static

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000
-
+//
 app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
