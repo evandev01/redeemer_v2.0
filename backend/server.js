255a291e41d3a68dcd967ff93688d2cb5932a7b0
Bears <evanbero1980@gmail.com>
2021-12-07 09:25:16 -0600

    static

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
 }
 
 const PORT = process.env.PORT || 5000
