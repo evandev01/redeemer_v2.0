622788c36bbf60a62fc8f1833d340c69a0096a2a
Bears <evanbero1980@gmail.com>
2021-12-07 10:36:23 -0600

    static

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')))
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 } else {
   app.get('/', (req, res) => {
