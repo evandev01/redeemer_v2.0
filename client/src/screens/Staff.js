b8776eb4d775594198c9ff91bba74e4e9dae8bef
Bears <evanbero1980@gmail.com>
2022-02-16 11:53:36 -0600

    updated live stream

--- client/src/screens/Staff.js
+++ client/src/screens/Staff.js
@@ -10,12 +10,13 @@ import Bob from '../assets/staff/IMG_2475.jpeg'
 const Staff = () => {
   return (
     <Fragment>
-      <Container id='staff'>
+      <Container id='staff' className='mt-5'>
         <Row className='justify-content-md-center'>
           <Col xs={12} lg={12} className='text-center'>
             <h4 id='title'>Our Servant Leaders</h4>
           </Col>
         </Row>
+        <Row id='live-border' />
 
         <Row className='justify-content-md-center'>
           <Col xs={12} lg={12} className='text-center'>
@@ -24,32 +25,27 @@ const Staff = () => {
         </Row>
 
         <Row className='justify-content-md-center'>
-          <Col md={5} className='text-center'>
+          <Col md={6} className='text-center'>
             <Image src={Joel} alt='Pastor Joel' id='photo' />
             <h6 id='staff-h6'>Pastor Joel Gilbert</h6>
           </Col>
-          <Col md={2} />
-          <Col md={5} className='text-center'>
+          {/* <Col md={2} /> */}
+          <Col md={6} className='text-center'>
             <Image src={Bob} alt='Bob - Elder' id='photo' />
             <h6 id='staff-h6'>Pastor Bob Wheeler</h6>
           </Col>
         </Row>
-
+        <Row id='live-border' />
         <Row className='justify-content-md-center'>
-          <Col md={12} className='text-center'>
-            <h5 id='staff-h5'>Deacons</h5>
-          </Col>
-        </Row>
-
-        <Row className='justify-content-md-center'>
-          <Col md={12} className='text-center'>
+          <Col md={6} className='text-center'>
             <Image src={Steve} alt='Steve - Elder' id='photo' />
             <h6 id='staff-h6'>Steve Renfrow</h6>
+            <p id='staff-position'>(Deacon)</p>
           </Col>
-        </Row>
+          {/* </Row>
 
-        <Row className='justify-content-md-center'>
-          <Col md={12} className='text-center'>
+        <Row className='justify-content-md-center'> */}
+          <Col md={6} className='text-center'>
             <Image
               src={David}
               alt={'David Harris - Worship Leader'}
@@ -61,7 +57,7 @@ const Staff = () => {
         </Row>
 
         <Row className='justify-content-md-center'>
-          <Col md={5} className='text-center'>
+          <Col md={6} className='text-center'>
             <Image
               src={Georgette}
               alt={'Georgette Aguilar - Church Treasurer'}
@@ -70,10 +66,10 @@ const Staff = () => {
             <h6 id='staff-h6'>Georgette Aguilar</h6>
             <p id='staff-position'>(Church Treasurer)</p>
           </Col>
-          <Col md={2} />
-          <Col md={5} className='text-center'>
+          {/* <Col md={2} /> */}
+          <Col md={6} className='text-center'>
             <Image src={Lisa} alt={'Lisa - Volunteer'} id='photo' />
-            <h6 id='staff-h6'>Lisa Brown</h6>
+            <h6 id='staff-h6'>Lisa Burke</h6>
             <p id='staff-position'>(Church Clerk)</p>
           </Col>
         </Row>
--- client/src/screens/WatchLive.js
+++ client/src/screens/WatchLive.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import '../index.css'
 
 const WatchLive = () => {
+  const h5Style = {
+    color: 'rgba(0, 0, 0, 0.685)',
+    // fontSize: 'large',
+    // fontWeight: 'none',
+    // textDecoration: 'underline',
+  }
   return (
     <Fragment>
       <Container id='stream-container'>
         {/* SUNDAY SERVICE */}
+        <Row id='live-border' />
         <Container>
+          <Row>
+            <Col className='text-center'>
+              <h5 style={h5Style}>Sunday Service</h5>
+            </Col>
+          </Row>
           <Row id='stream-player' className='justify-content-center'>
             <Col md='auto' />
             <Col xs={12} sm={12} md={10} lg={10} className='text-center'>
@@ -27,6 +40,11 @@ const WatchLive = () => {
 
         {/* WEDNESDAY SERVICE */}
         <Container>
+          <Row>
+            <Col className='text-center'>
+              <h5 style={h5Style}>Wednesday Service</h5>
+            </Col>
+          </Row>
           <Row id='stream-player' className='justify-content-center'>
             <Col md='auto' />
             <Col xs={12} sm={12} md={10} lg={10} className='text-center'>
@@ -34,7 +52,7 @@ const WatchLive = () => {
                 id='player'
                 width='560'
                 height='315'
-                src='https://www.youtube.com/embed/ocfQ_ctmo9c'
+                src='https://www.youtube.com/embed/z9Pm_PxXe2g'
                 title='YouTube video player'
                 frameborder='0'
                 allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
@@ -43,7 +61,7 @@ const WatchLive = () => {
             </Col>
           </Row>
         </Container>
-        {/* <Row id='live-border' /> */}
+        <Row id='live-border' />
       </Container>
     </Fragment>
   )
