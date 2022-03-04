4966c581ebef713cdd8aed7c570b22be65a64d80
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-03-04 16:07:37 -0600

    removed former events file, Events.js and renamed Event.js to Events.js, added confirmation message to deleteHandler

--- client/src/screens/Events/Events.js
+++ client/src/screens/Events/Events.js
@@ -1,110 +1,259 @@
-import React from 'react'
-import { Row, Col, Image } from 'react-bootstrap'
-import Shoebox from '../../assets/events/shoebox.jpg'
-import FoodDrive from '../../assets/events/food_drive.png'
-import Bible from '../../assets/events/Bible.jpg'
-import WOM from '../../assets/events/wom.jpg'
-import BYBC from '../../assets/events/Bible.jpg'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, Button } from 'react-bootstrap'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import EventModal from '../../components/EventModal'
+import { listEvents, deleteEvent, updateEvent } from '../../actions/event'
+import '../../index.css'
 
-const Events = () => {
+const Event = () => {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const eventList = useSelector(state => state.eventList)
+  const { loading, error, events } = eventList
+
+  const eventCreate = useSelector(state => state.eventCreate)
+  const { success: successCreate } = eventCreate
+
+  const eventDelete = useSelector(state => state.eventDelete)
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = eventDelete
+
+  const eventUpdate = useSelector(state => state.eventUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = eventUpdate
+
+  useEffect(() => {
+    dispatch(listEvents())
+  }, [dispatch, successDelete, successUpdate, successCreate])
+
+  const deleteHandler = async (e, result, deletedEvent, id) => {
+    e.preventDefault()
+    if (result) {
+      const dTier = deletedEvent.tier
+
+      // Filter all events with a tier greater than the deleted tier and update tiers - 1
+      // This ensures that there will always be a tier 0 and no gaps in the tier numbers
+      const tEvents = events.filter(event => event.tier > dTier)
+
+      const updateTiers = () => {
+        tEvents.map(event =>
+          dispatch(
+            updateEvent({
+              _id: event._id,
+              title: event.title,
+              line1: event.line1,
+              line2: event.line2,
+              desc: event.desc,
+              desc2: event.desc2,
+              image: event.image,
+              tier: event.tier - 1,
+            })
+          )
+        )
+      }
+      await updateTiers()
+      dispatch(deleteEvent(id))
+    } else return
+  }
+
+  const upHandler = async (e, event, i) => {
+    e.preventDefault()
+    await events.forEach((data, index) => {
+      if (index === i - 1) {
+        dispatch(
+          updateEvent({
+            _id: data._id,
+            title: data.title,
+            line1: data.line1,
+            line2: data.line2,
+            desc: data.desc,
+            desc2: data.desc2,
+            image: data.image,
+            tier: data.tier + 1,
+          })
+        )
+      }
+    })
+    dispatch(
+      updateEvent({
+        _id: event._id,
+        title: event.title,
+        line1: event.line1,
+        line2: event.line2,
+        desc: event.desc,
+        desc2: event.desc2,
+        image: event.image,
+        tier: event.tier - 1,
+      })
+    )
+  }
+
+  const downHandler = async (e, event, i) => {
+    e.preventDefault()
+    await events.forEach((data, index) => {
+      if (index === i + 1) {
+        dispatch(
+          updateEvent({
+            _id: data._id,
+            title: data.title,
+            line1: data.line1,
+            line2: data.line2,
+            desc: data.desc,
+            desc2: data.desc2,
+            image: data.image,
+            tier: data.tier - 1,
+          })
+        )
+      }
+    })
+    dispatch(
+      updateEvent({
+        _id: event._id,
+        title: event.title,
+        line1: event.line1,
+        line2: event.line2,
+        desc: event.desc,
+        desc2: event.desc2,
+        image: event.image,
+        tier: event.tier + 1,
+      })
+    )
+  }
+
   return (
-    <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-      <Row id='live-border' />
-      {/* MENS BIBLE STUDY */}
-      <Row className='mt-5'>
-        <Col id='events-col' className='text-center' xs={12} sm={12} lg={6}>
-          <Image id='events-graphic' src={Bible} alt="Men's Bible Study" />
-        </Col>
-        <Col xs={12} sm={12} lg={6} className='text-center'>
-          <h3>Mens Bible Study</h3>
-          <p>
-            <strong>- Saturday Mornings -</strong>
-            <br />
-            <strong>7:00am - 8:30am</strong> through <strong>March 12th</strong>
-          </p>
-        </Col>
-      </Row>
-
+    <div id='container-div'>
+      {loading && <Loader />}
+      {loadingUpdate && <Loader />}
+      {loadingDelete && <Loader />}
+      {error && <Message variant='danger'>{error}</Message>}
+      {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
       <Row id='live-border' />
 
-      {/* WOM */}
-      <Row>
-        <Col id='events-col' className='text-center' xs={12} sm={12} lg={6}>
-          <Image id='events-graphic' src={WOM} alt='WOM' />
-        </Col>
-        <Col xs={12} sm={12} lg={6} className='text-center'>
-          <h3 className='text-center'>WOM</h3>
-          <p id='events-text' className='text-center'>
-            <strong>- Tuesday, March 1st -</strong>
-            <br />
-            <strong>6:30pm</strong>
-          </p>
-          <p id='events-text'>
-            For further details, please <a href='/connect'>contact us</a>.
-          </p>
-        </Col>
-      </Row>
+      {userInfo && (
+        <>
+          <Row>
+            <Col className='text-center'>
+              <Button onClick={handleShow}>
+                <i className='fa-solid fa-circle-plus' /> Create New
+              </Button>
+            </Col>
+          </Row>
+          <Row id='live-border' />
+        </>
+      )}
 
-      <Row id='live-border' />
+      {show && <EventModal show={show} handleClose={handleClose} />}
 
-      {/* FOOD DRIVE */}
-      <Row>
-        <Col id='events-col' className='text-center' xs={12} sm={12} lg={6}>
-          <Image id='events-graphic' src={FoodDrive} alt='Food drive' />
-        </Col>
-        <Col xs={12} sm={12} lg={6} className='text-center'>
-          <h3 className='text-center'>Food Drive</h3>
-          <p id='events-text' className='text-center'>
-            <strong>- March -</strong>
-            <br />
-            Items: <strong>Hearty Soups with Meat</strong>
-            <br />
-            (Please look for 2023 expirations)
-          </p>
-        </Col>
-      </Row>
-
-      <Row id='live-border' />
-
-      {/* SHOEBOX COLLECTION */}
-      <Row>
-        <Col id='events-col' className='text-center'>
-          <Image id='events-graphic' src={Shoebox} alt='Shoebox collection' />
-        </Col>
-        <Col xs={12} sm={12} lg={6} className='text-center'>
-          <h3 className='text-center'>Shoebox Collection</h3>
-          <p id='events-text' className='text-center'>
-            <strong>- March -</strong>
-            <br />
-            Items: <strong>Handcrafted Items</strong>
-          </p>
-          <p id='events-text'>
-            For further details, please <a href='/connect'>contact us</a>.
-          </p>
-        </Col>
-      </Row>
-
-      <Row id='live-border' />
-
-      {/* BACKYARD BIBLE CLUB */}
-      <Row>
-        <Col id='events-col' className='text-center' xs={12} sm={12} lg={6}>
-          <Image id='events-graphic' src={BYBC} alt='Backyard Bible Club' />
-        </Col>
-        <Col xs={12} sm={12} lg={6} className='text-center'>
-          <h3 className='text-center'>Backyard Bible Club</h3>
-          <p id='events-text' className='text-center'>
-            <strong>
-              Sign up Sheet in Foyer for families interested in hosting or
-              helping with Backyard Bible Club in their Neighborhood in August.
-            </strong>
-          </p>
-        </Col>
-      </Row>
-
-      <Row id='live-border' />
+      {events &&
+        events
+          .sort((a, b) => {
+            return a.tier - b.tier
+          })
+          .map((event, i) => (
+            <div key={i}>
+              <Row className='mt-5'>
+                <Col
+                  id='events-col'
+                  className='text-center'
+                  xs={12}
+                  sm={12}
+                  lg={6}
+                >
+                  <Image
+                    id='events-graphic'
+                    alt={event.title}
+                    src={event.image}
+                  />
+                </Col>
+                <Col xs={12} sm={12} lg={6} className='text-center'>
+                  <h3>{event.title}</h3>
+                  <p>
+                    <strong>{event.line1}</strong>
+                    <br />
+                    {event.line2 && (
+                      <>
+                        <strong>{event.line2}</strong>
+                        <br />
+                      </>
+                    )}
+                    {event.desc && (
+                      <>
+                        <strong>{event.desc}</strong>
+                        <br />
+                      </>
+                    )}
+                    {event.desc2 && (
+                      <>
+                        <span>{event.desc2}</span>
+                        <br />
+                      </>
+                    )}
+                  </p>
+                </Col>
+              </Row>
+              {userInfo && (
+                <Row>
+                  <Col className='text-center'>
+                    <Button
+                      disabled={i === 0}
+                      className='px-3 m-3'
+                      onClick={e => upHandler(e, event, i)}
+                    >
+                      <i class='fa-solid fa-angle-up'></i> Move Up
+                    </Button>
+                  </Col>
+                  <Col className='text-center'>
+                    <Button
+                      disabled={i === events.length - 1}
+                      className='px-3 m-3'
+                      onClick={e => downHandler(e, event, i)}
+                    >
+                      Move Down <i class='fa-solid fa-angle-down'></i>
+                    </Button>
+                  </Col>
+                  <Col className='text-center'>
+                    <Link to={`/event/edit/${event._id}`}>
+                      <Button className='px-3 m-3' variant='warning'>
+                        <i className='fa-solid fa-pen-to-square' /> Edit
+                      </Button>
+                    </Link>
+                  </Col>
+                  <Col className='text-center'>
+                    <Button
+                      className='px-3 m-3'
+                      variant='danger'
+                      onClick={e => {
+                        const result = window.confirm('Are you sure?')
+                        deleteHandler(e, result, event, event._id)
+                      }}
+                    >
+                      <i className='fa-solid fa-trash-can' /> Delete
+                    </Button>
+                  </Col>
+                </Row>
+              )}
+              <Row id='live-border' />
+            </div>
+          ))}
     </div>
   )
 }
 
-export default Events
+export default Event
