57d018f158e19720f0a1bafdc18f24e98c33ec2c
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-03-06 19:52:38 -0600

    tested security

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -59,7 +59,6 @@ export const login = (email, password) => async dispatch => {
 export const logout = () => dispatch => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
-  // document.location.href = '/login'
 }
 
 export const register = (name, email, password) => async dispatch => {
