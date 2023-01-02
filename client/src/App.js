3152350108863166be1f7d1b442231bd15236e78
Evan Bero <63209633+evandev01@users.noreply.github.com>
2023-01-02 15:10:37 -0600

    Removed giving

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { Container } from 'react-bootstrap'
 import './index.css'
 import Home from './screens/Home'
 import Connect from './screens/Connect'
-import Giving from './screens/Giving'
 import Staff from './screens/Staff'
 import WatchLive from './screens/WatchLive'
 import About from './screens/About'
@@ -17,32 +16,31 @@ import Instructions from './screens/Events/Instructions'
 import Images from './screens/Events/Images'
 
 const App = () => {
-  return (
-    <Router>
-      <div id='wrapper'>
-        <Header />
-        <main className='p-3'>
-          <Container>
-            <NavTabs />
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/connect' element={<Connect />} />
-              <Route path='/giving' element={<Giving />} />
-              <Route path='/staff' element={<Staff />} />
-              <Route path='/watchlive' element={<WatchLive />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/events' element={<Events />} />
-              <Route path='/instructions' element={<Instructions />} />
-              <Route path='/event/edit' element={<EventEdit />} />
-              <Route path='/event/edit/:id' element={<EventEdit />} />
-              <Route path='/images' element={<Images />} />
-            </Routes>
-          </Container>
-        </main>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<div id='wrapper'>
+				<Header />
+				<main className='p-3'>
+					<Container>
+						<NavTabs />
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/connect' element={<Connect />} />
+							<Route path='/staff' element={<Staff />} />
+							<Route path='/watchlive' element={<WatchLive />} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/events' element={<Events />} />
+							<Route path='/instructions' element={<Instructions />} />
+							<Route path='/event/edit' element={<EventEdit />} />
+							<Route path='/event/edit/:id' element={<EventEdit />} />
+							<Route path='/images' element={<Images />} />
+						</Routes>
+					</Container>
+				</main>
+			</div>
+		</Router>
+	)
 }
 
 export default App
--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -6,48 +6,47 @@ import '../index.css'
 import { logout } from '../actions/user'
 
 const NavTabs = () => {
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-  const userLogin = useSelector(state => state.userLogin)
-  const { userInfo } = userLogin
+	const userLogin = useSelector(state => state.userLogin)
+	const { userInfo } = userLogin
 
-  const logoutHandler = () => {
-    dispatch(logout())
-    navigate('/')
-  }
-  return (
-    <>
-      <Container
-        id='nav-container'
-        className='justify-content-center'
-        style={{ fontWeight: '700' }}
-      >
-        <Row className='justify-content-md-center'>
-          <Col xs={1} md={1} lg={4} />
-          <Col xs={10} md={10} lg={4}>
-            <Navbar expand='md' className='justify-content-md-center'>
-              <Navbar.Toggle aria-controls='basic-navbar-nav' />
-              <Navbar.Collapse id='basic-navbar-nav'>
-                <Navbar>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/'>
-                      HOME
-                    </Nav.Link>
-                  </Nav.Item>
-                </Navbar>
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/about'>
-                      ABOUT
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
+	const logoutHandler = () => {
+		dispatch(logout())
+		navigate('/')
+	}
+	return (
+		<>
+			<Container
+				id='nav-container'
+				className='justify-content-center'
+				style={{ fontWeight: '700' }}>
+				<Row className='justify-content-md-center'>
+					<Col xs={1} md={1} lg={4} />
+					<Col xs={10} md={10} lg={4}>
+						<Navbar expand='md' className='justify-content-md-center'>
+							<Navbar.Toggle aria-controls='basic-navbar-nav' />
+							<Navbar.Collapse id='basic-navbar-nav'>
+								<Navbar>
+									<Nav.Item>
+										<Nav.Link id='link' href='/'>
+											HOME
+										</Nav.Link>
+									</Nav.Item>
+								</Navbar>
+								<Nav>
+									<Nav.Item>
+										<Nav.Link id='link' href='/about'>
+											ABOUT
+										</Nav.Link>
+									</Nav.Item>
+								</Nav>
 
-                <Nav>
-                  <Nav.Item>
-                    {/* <Badge
+								<Nav>
+									<Nav.Item>
+										{/* <Badge
                       bg='secondary'
                       style={{
                         color: 'rgba(126, 240, 82, 0.795)',
@@ -55,73 +54,64 @@ const NavTabs = () => {
                       className='text-center'
                     >
                       New */}
-                    <Nav.Link
-                      // style={{ color: 'white' }}
-                      id='link'
-                      href='/events'
-                    >
-                      EVENTS
-                    </Nav.Link>
-                    {/* </Badge> */}
-                  </Nav.Item>
-                </Nav>
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/watchlive'>
-                      WATCH
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/connect'>
-                      CONNECT
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/giving'>
-                      GIVING
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                <Nav>
-                  <Nav.Item>
-                    <Nav.Link id='link' href='/staff'>
-                      STAFF
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                {userInfo && userInfo !== null ? (
-                  <Nav>
-                    <Nav.Item>
-                      <Nav.Link
-                        id='link'
-                        onClick={logoutHandler}
-                        style={{ color: 'blue' }}
-                      >
-                        LOGOUT
-                      </Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                ) : (
-                  <Nav>
-                    <Nav.Item>
-                      <Nav.Link id='link' href='/login'>
-                        ADMIN
-                      </Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                )}
-              </Navbar.Collapse>
-            </Navbar>
-          </Col>
-          <Col xs={1} md={1} lg={4} />
-        </Row>
-      </Container>
-    </>
-  )
+										<Nav.Link
+											// style={{ color: 'white' }}
+											id='link'
+											href='/events'>
+											EVENTS
+										</Nav.Link>
+										{/* </Badge> */}
+									</Nav.Item>
+								</Nav>
+								<Nav>
+									<Nav.Item>
+										<Nav.Link id='link' href='/watchlive'>
+											WATCH
+										</Nav.Link>
+									</Nav.Item>
+								</Nav>
+								<Nav>
+									<Nav.Item>
+										<Nav.Link id='link' href='/connect'>
+											CONNECT
+										</Nav.Link>
+									</Nav.Item>
+								</Nav>
+								<Nav>
+									<Nav.Item>
+										<Nav.Link id='link' href='/staff'>
+											STAFF
+										</Nav.Link>
+									</Nav.Item>
+								</Nav>
+								{userInfo && userInfo !== null ? (
+									<Nav>
+										<Nav.Item>
+											<Nav.Link
+												id='link'
+												onClick={logoutHandler}
+												style={{ color: 'blue' }}>
+												LOGOUT
+											</Nav.Link>
+										</Nav.Item>
+									</Nav>
+								) : (
+									<Nav>
+										<Nav.Item>
+											<Nav.Link id='link' href='/login'>
+												ADMIN
+											</Nav.Link>
+										</Nav.Item>
+									</Nav>
+								)}
+							</Navbar.Collapse>
+						</Navbar>
+					</Col>
+					<Col xs={1} md={1} lg={4} />
+				</Row>
+			</Container>
+		</>
+	)
 }
 
 export default NavTabs
